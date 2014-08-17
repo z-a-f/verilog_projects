e4654eaf006ff2b68975a1f64786981e9fce15e4
Zafar Takhirov <z.tahirov@gmail.com>
2014-08-16 20:04:46 -0400

    AUTOCOMMIT: Sat Aug 16 20:04:46 EDT 2014

--- KSA/KSA_always.v
+++ KSA/KSA_always.v
@@ -1,36 +1,36 @@
 module KSA_pipe(s, a, b, c, clk);
-	parameter BITS = 64;
-	parameter LEVELS = 6;
-
-	// IO's
-	input [BITS-1:0]	a;
-	input [BITS-1:0]	b;
-	input				c;
-
-	input clk;
-
-	output [BITS:0]		s;
-
-	// Wires:
-	wire [BITS-1:0]		aIn;
-	wire [BITS-1:0]		bIn;
-	wire				cIn;
-
-	wire [BITS:0]		sOut;
-
-	// Input Buffers:
-	always @ (posedge clk) begin
-		aIn <= a;
-		bIn <= b;
-		cIn <= c;
-	end
-
-	// Adder:
-	KSA #(.BITS(BITS), .LEVELS(LEVELS))adder4 (sOut, aIn, bIn, cIn);
-
-	// Output Buffers:
-	always @ (posedge clk) begin
-		s <= sOut;
-	end
-
+   parameter BITS = 64;
+   parameter LEVELS = 6;
+   
+   // IO's
+   input [BITS-1:0]	a;
+   input [BITS-1:0] 	b;
+   input 		c;
+   
+   input 		clk;
+   
+   output [BITS:0] 	s;
+   
+   // Wires:
+   wire [BITS-1:0] aIn;
+   wire [BITS-1:0] bIn;
+   wire 	   cIn;
+   
+   wire [BITS:0]   sOut;
+   
+   // Input Buffers:
+   always @ (posedge clk) begin
+      aIn <= a;
+      bIn <= b;
+      cIn <= c;
+   end
+   
+   // Adder:
+   KSA #(.BITS(BITS), .LEVELS(LEVELS))adder4 (sOut, aIn, bIn, cIn);
+   
+   // Output Buffers:
+   always @ (posedge clk) begin
+      s <= sOut;
+   end
+   
 endmodule
