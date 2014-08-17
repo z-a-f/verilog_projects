9692a6cc9789c16f2f98f6e6c61403927063d5bd
Zafar Takhirov <z.tahirov@gmail.com>
2014-08-16 20:16:04 -0400

    AUTOCOMMIT: Sat Aug 16 20:16:04 EDT 2014

--- KSA/KSA_pipe.v
+++ KSA/KSA_pipe.v
@@ -1,63 +1,65 @@
-
+/* This version uses explicit BUFFERS
+ * Replace the buffers with RTL
+ */
 
 module KSA_pipe(s, a, b, c, clk);
-	parameter BITS = 64;
-	parameter LEVELS = 6;
+   parameter BITS = 64;
+   parameter LEVELS = 6;
 
-	// IO's
-	input [BITS-1:0]	a;
-	input [BITS-1:0]	b;
-	input				c;
+   // IO's
+   input [BITS-1:0]	a;
+   input [BITS-1:0] 	b;
+   input 		c;
 
-	input clk;
+   input 		clk;
 
-	output [BITS:0]		s;
+   output [BITS:0] 	s;
 
-	// Wires:
-	wire [BITS-1:0]		aIn;
-	wire [BITS-1:0]		bIn;
-	wire				cIn;
+   // Wires:
+   wire [BITS-1:0] 	aIn;
+   wire [BITS-1:0] 	bIn;
+   wire 		cIn;
 
-	wire [BITS:0]		sOut;
+   wire [BITS:0] 	sOut;
 
 
 
-	// Input Buffers:
-	REGS #(.BITS(BITS)) inputBUF_A (aIn, a, clk);
-	REGS #(.BITS(BITS)) inputBUF_B (bIn, b, clk);
-	REG 				inputBUF_C (cIn, c, clk);
+   // Input Buffers:
+   REGS #(.BITS(BITS)) inputBUF_A (aIn, a, clk);
+   REGS #(.BITS(BITS)) inputBUF_B (bIn, b, clk);
+   REG 			inputBUF_C (cIn, c, clk);
 
-	// Adder:
-	KSA #(.BITS(BITS), .LEVELS(LEVELS))adder4 (sOut, aIn, bIn, cIn);
+   // Adder:
+   KSA #(.BITS(BITS), .LEVELS(LEVELS))adder4 (sOut, aIn, bIn, cIn);
 
-	// Output Buffers:
-	REGS #(.BITS(BITS+1)) outputBUF_S (s, sOut, clk);
+   // Output Buffers:
+   REGS #(.BITS(BITS+1)) outputBUF_S (s, sOut, clk);
 
 endmodule
 
 module REGS (Q, D, clk);
-	parameter BITS = 64;
-	input [BITS-1:0]	D;
-	input				clk;
-	output [BITS-1:0]	Q;
+   parameter BITS = 64;
+   input [BITS-1:0]	D;
+   input 		clk;
+   output [BITS-1:0] 	Q;
 
-	genvar i;
-	generate
-		for (i = 0; i < BITS; i = i + 1) begin
-			REG RR (Q[i], D[i], clk);
-		end
+   genvar 		i;
+   generate
+      for (i = 0; i < BITS; i = i + 1) begin
+	 REG RR (Q[i], D[i], clk);
+      end
 
-	endgenerate
+   endgenerate
 
 endmodule
 
 module REG (Q, D, clk);
-	input		D;
-	input 		clk;
-	output reg 	Q;
+   input		D;
+   input 		clk;
+   output reg 		Q;
 
-	always @ (posedge clk) begin
-		Q <= D;
-	end
+   always @ (posedge clk) begin
+      Q <= D;
+   end
 
 endmodule
--- KSA/KSA_test1.sv
+++ KSA/KSA_test1.sv
@@ -5,45 +5,45 @@
  ************************************/
 
 module test;
-  parameter BITS = 16;
-  parameter LEVELS = $floor($clog2(BITS));
-  // I am not sure if the above is synthesizable
-  // If not, calculate the levels manually, using
-  // LEVELS = floor(log2(BITS))
+   parameter BITS = 16;
+   parameter LEVELS = $floor($clog2(BITS));
+   // I am not sure if the above is synthesizable
+   // If not, calculate the levels manually, using
+   // LEVELS = floor(log2(BITS))
 
-  // IO's
-  reg [BITS-1:0]    a;
-  reg [BITS-1:0]    b;
+   // IO's
+   reg [BITS-1:0]    a;
+   reg [BITS-1:0]    b;
 
-  // Results:
-  wire [BITS:0] s_KSA;
-  wire [BITS:0] s_BEHAV;
+   // Results:
+   wire [BITS:0]     s_KSA;
+   wire [BITS:0]     s_BEHAV;
 
-  reg error = 1'b0;   // This is just for the stop condition (verificaiton)
+   reg 		     error = 1'b0;   // This is just for the stop condition (verificaiton)
 
-  initial begin
-    //$dumpfile("dump.vcd");
-    //$dumpvars(1, test);
-    a = 0;
-    b = 1;
-    // #1_000_000_000 $finish;
-  end
+   initial begin
+      //$dumpfile("dump.vcd");
+      //$dumpvars(1, test);
+      a = 0;
+      b = 1;
+      // #1_000_000_000 $finish;
+   end
 
-  KSA #(.BITS(BITS), .LEVELS(LEVELS))adder4 (s_KSA, a, b, 1'b0);
+   KSA #(.BITS(BITS), .LEVELS(LEVELS))adder4 (s_KSA, a, b, 1'b0);
 
-  assign s_BEHAV = a + b;   // This is the baseline - will be compared to the DUT
+   assign s_BEHAV = a + b;   // This is the baseline - will be compared to the DUT
 
-  initial begin
-    while (error == 1'b0) begin
-      #1 {a, b} = {a, b} + 1'b1;    // Keep on incrementing
-      if (s_BEHAV != s_KSA) begin   // Until the results differ
-        $display ("Invalid: %d+%d = %d (not %d)", a, b, s_BEHAV, s_KSA);
-        error = 1'b1;
+   initial begin
+      while (error == 1'b0) begin
+	 #1 {a, b} = {a, b} + 1'b1;    // Keep on incrementing
+	 if (s_BEHAV != s_KSA) begin   // Until the results differ
+            $display ("Invalid: %d+%d = %d (not %d)", a, b, s_BEHAV, s_KSA);
+            error = 1'b1;
+	 end
+	 if (a == 0 && b == 0)       // If all the values checked, $finish
+           #1 $finish;
+	 // $display ("a = %d", a);
       end
-      if (a == 0 && b == 0)       // If all the values checked, $finish
-        #1 $finish;
-      // $display ("a = %d", a);
-    end
 
-  end
+   end
 endmodule
